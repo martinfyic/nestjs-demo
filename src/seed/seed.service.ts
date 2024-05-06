df78fcb8b326ea818fbc5195de3e4048e189abdd
martinfyic <martin.f.yic@gmail.com>
2024-05-06 18:51:26 -0300

    refactor: :recycle: create http-adapter to remove axios dependencie

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
 import { PokemonToInsert, PokeResponse } from './interfaces';
+import { AxiosAdapter } from 'src/common/http-adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({});
+
     const pokemonToInsert: PokemonToInsert[] = [];
 
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
     data.results.forEach(async ({ name, url }) => {
