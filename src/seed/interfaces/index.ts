891067fec52a5f4d4478fa87a2d9aa281b81371e
martinfyic <martin.f.yic@gmail.com>
2024-05-06 18:12:29 -0300

    feat: :seedling: add interface to seed module

---
+++ src/seed/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { PokeResponse } from './poke-response.interface';
+export { PokemonToInsert } from './pokemon-to-insert.interface';
---
+++ src/seed/interfaces/pokemon-to-insert.interface.ts
@@ -0,0 +1,5 @@
+export interface PokemonToInsert {
+  name: string;
+  no: number;
+  url: string;
+}
